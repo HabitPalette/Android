3bea5b3212062745e8dacaae471fe6a7a520de03
soeun lee <thdmsdl87@gmail.com>
2022-04-27 17:50:33 +0900

    Refactor createHabitPeriodSeekBar

--- app/src/main/java/com/example/habitpalette/ui/common/CreateHabitPeriodSeekBar.kt
+++ app/src/main/java/com/example/habitpalette/ui/common/CreateHabitPeriodSeekBar.kt
@@ -1,72 +1,58 @@
 package com.example.habitpalette.ui.common
 
-import android.R
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 import androidx.appcompat.widget.AppCompatSeekBar
-import android.os.Build
 import android.util.AttributeSet
 import java.util.ArrayList
+import androidx.annotation.AttrRes
+import com.example.habitpalette.R
 import kotlin.jvm.Synchronized
 
-class CreateHabitPeriodSeekBar(
-    private var mContext: Context,
-    attrs: AttributeSet?,
-    defStyleAttr: Int
-) : AppCompatSeekBar(
-    mContext, attrs, defStyleAttr
-) {
+class CreateHabitPeriodSeekBar constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = R.attr.seekBarStyle
+) : AppCompatSeekBar(context, attrs, defStyleAttr) {
+
     private var mRangeCount = 0
-    private var mPaint: Paint? = null
+    private var mPaint = Paint()
     private var mProgressStrings: ArrayList<String>? = null
 
-    constructor(context: Context, attrs: AttributeSet?) : this(
-        context,
-        attrs,
-        R.attr.seekBarStyle
-    ) {
-        mContext = context
-        init()
-    }
-
-    private fun init() {
-        mPaint = Paint()
-        mPaint!!.color = Color.BLACK
-        mPaint!!.textSize = 44f
-        mPaint!!.isAntiAlias = true
-        mPaint!!.textAlign = Paint.Align.CENTER
-
-        //Api21 and above call, remove the background behind the slider
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            splitTrack = false
+    init {
+        mPaint = Paint().apply {
+            color = Color.BLACK
+            textSize = 44f
+            isAntiAlias = true
+            textAlign = Paint.Align.CENTER
         }
     }
 
-    private fun dp2px(dp: Float): Int {
-        return (mContext.resources.displayMetrics.density * dp + 0.5f).toInt()
-    }
+    private fun dp2px(dp: Float): Int  = (context.resources.displayMetrics.density * dp + 0.5f).toInt()
 
     @Synchronized
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+
         if (width <= 0 || mRangeCount <= 0) {
             return
         }
+
         for (i in 0 until mRangeCount) {
             val bounds = Rect()
-            val curText = mProgressStrings!![i]
+            val curText = mProgressStrings?.get(i)
             if (curText != null) {
                 val textLength = curText.length
-                mPaint!!.getTextBounds(curText, 0, textLength, bounds)
+                mPaint.getTextBounds(curText, 0, textLength, bounds)
                 val interval = (width - paddingLeft - paddingRight) * (i.toFloat() / max)
                 val textY =
                     (height - paddingBottom - paddingTop) / 2.toFloat() + bounds.height() / 2.toFloat() - dp2px(
                         2f
                     )
-                canvas.drawText(curText, interval, textY, mPaint!!)
+                canvas.drawText(curText, interval, textY, mPaint)
             }
         }
     }
@@ -79,9 +65,5 @@ class CreateHabitPeriodSeekBar(
     fun setStrings(progressStrings: ArrayList<String>?) {
         mProgressStrings = progressStrings
         requestLayout()
-    }
-
-    init {
-        init()
     }
 }
