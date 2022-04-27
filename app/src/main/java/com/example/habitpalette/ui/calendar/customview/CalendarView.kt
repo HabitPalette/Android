364073a9fcaa2f972f3f8e4f26aa204ee8ade9e9
soeun lee <thdmsdl87@gmail.com>
2022-04-27 17:50:51 +0900

    Add Calendar custom view

--- app/src/main/java/com/example/habitpalette/ui/calendar/customview/CalendarView.kt
+++ app/src/main/java/com/example/habitpalette/ui/calendar/customview/CalendarView.kt
@@ -1,4 +1,22 @@
 package com.example.habitpalette.ui.calendar.customview
 
-class CalendarView {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ContextThemeWrapper
+import android.view.ViewGroup
+import androidx.annotation.AttrRes
+import androidx.annotation.StyleRes
+import com.example.habitpalette.R
+
+class CalendarView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = R.attr.calendarViewStyle,
+    @StyleRes defStyleRes: Int = R.style.Calendar_CalendarViewStyle
+) : ViewGroup(ContextThemeWrapper(context, defStyleRes), attrs, defStyleAttr) {
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
