305bb760705a1fe45880a6546a4c767236b6e54d
soeun lee <thdmsdl87@gmail.com>
2022-05-21 14:28:48 +0900

    Refactor: Refactor RecyclerView to ListView
    Add DiffUtil for replace notifyDataSetChanged()

--- app/src/main/java/com/example/habitpalette/ui/home/CurrentHabitRecyclerAdapter.kt
+++ app/src/main/java/com/example/habitpalette/ui/home/CurrentHabitRecyclerAdapter.kt
@@ -1,49 +1,34 @@
 package com.example.habitpalette.ui.home
 
-import android.content.Context
+import android.annotation.SuppressLint
 import android.os.Build
-import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import android.view.ViewGroup
 import android.view.LayoutInflater
-import android.view.View
 import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import com.example.habitpalette.data.model.Habit
 import com.example.habitpalette.databinding.ItemCurrentHabitBinding
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
-class CurrentHabitRecyclerAdapter(
-    val context: Context,
-    var presentList: List<Habit>,
-    val onClickDetailButton: () -> Unit) :
-    RecyclerView.Adapter<CurrentHabitRecyclerAdapter.CurrentHabitViewHolder>() {
+class CurrentHabitRecyclerAdapter:
+    ListAdapter<Habit, CurrentHabitRecyclerAdapter.CurrentHabitViewHolder>(CurrentHabitComparator()){
 
     private lateinit var itemBinding: ItemCurrentHabitBinding
 
     inner class CurrentHabitViewHolder(private val itemBinding: ItemCurrentHabitBinding): RecyclerView.ViewHolder(itemBinding.root){
+        private var habit:Habit? = null
+
+        // item 클릭해서 CalendarActivity로 intent
+
+        @SuppressLint("SetTextI18n")
         @RequiresApi(Build.VERSION_CODES.O)
         fun bind(data: Habit) {
-//            if(adapterPosition==0 || adapterPosition == presentList.size-1){
-//                itemBinding.layoutCurrentItem.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
-//
-//                val displayMetrics = context.resources.displayMetrics
-//                val screenWidth = displayMetrics.widthPixels
-//                var mLayoutParam : RecyclerView.LayoutParams =  itemBinding.layoutCurrentItem.layoutParams as RecyclerView.LayoutParams
-//                if(adapterPosition == 0)
-//                    mLayoutParam.leftMargin = (screenWidth - itemBinding.layoutCurrentItem.measuredWidthAndState)/2
-//                else
-//                    mLayoutParam.rightMargin = (screenWidth - itemBinding.layoutCurrentItem.measuredWidthAndState)/2
-//            }
-
             itemBinding.tvTitle.text = data.title
             itemBinding.tvPeriod.text = ChronoUnit.DAYS.between(data.start_date, LocalDate.now()).toString()+"일째"
             itemBinding.pvCurrentProgress.percent = 50f
-
-            // item 클릭해서 CalendarActivity로 intent
-            itemBinding.layoutCurrentItem.setOnClickListener {
-                onClickDetailButton.invoke()
-            }
         }
     }
 
@@ -54,16 +39,17 @@ class CurrentHabitRecyclerAdapter(
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: CurrentHabitViewHolder, position: Int) {
-        holder.bind(presentList[position])
-        Log.d("CURRENT ADAPTER", presentList.toString())
+        holder.bind(getItem(position))
     }
 
-    override fun getItemCount(): Int {
-        return presentList.size
-    }
+    // Room을 사용할 경우 diffUtil로 notifyFataSetChanged 대체 가능
+    class CurrentHabitComparator: DiffUtil.ItemCallback<Habit>(){
+        override fun areItemsTheSame(oldItem: Habit, newItem: Habit): Boolean {
+            return oldItem === newItem
+        }
 
-    fun setData(presentHabitList: List<Habit>){
-        presentList = presentHabitList
-        notifyDataSetChanged()
+        override fun areContentsTheSame(oldItem: Habit, newItem: Habit): Boolean {
+            return oldItem.id == newItem.id
+        }
     }
 }
--- app/src/main/java/com/example/habitpalette/ui/home/PastHabitRecyclerAdapter.kt
+++ app/src/main/java/com/example/habitpalette/ui/home/PastHabitRecyclerAdapter.kt
@@ -1,34 +1,31 @@
 package com.example.habitpalette.ui.home
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import android.view.ViewGroup
 import android.view.LayoutInflater
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import com.example.habitpalette.data.model.Habit
 import com.example.habitpalette.databinding.ItemPastHabitBinding
 
-class PastHabitRecyclerAdapter(
-    val context: Context,
-    var pastList: List<Habit>,
-    val onClickDetailButton: () -> Unit) :
-    RecyclerView.Adapter<PastHabitRecyclerAdapter.PastHabitViewHolder>() {
+class PastHabitRecyclerAdapter:
+    ListAdapter<Habit, PastHabitRecyclerAdapter.PastHabitViewHolder>(PastHabitComparator()){
 
     private lateinit var itemBinding: ItemPastHabitBinding
 
     inner class PastHabitViewHolder(private val itemBinding: ItemPastHabitBinding): RecyclerView.ViewHolder(itemBinding.root){
+        private var habit:Habit? = null
+
+        // item 클릭해서 CalendarActivity로 intent
+
         @SuppressLint("SetTextI18n")
         fun bind(data: Habit) {
+            this.habit = data
             itemBinding.tvTitle.text = data.title
             itemBinding.tvScore.text = data.score.toString()
-            itemBinding.tvPeriod.text = data.start_date.toString()+"~"+data.end_date.toString()
+            itemBinding.tvPeriod.text = data.start_date.toString() + "~" + data.end_date.toString()
             itemBinding.pvPastProgress.percent = 50f
-
-            // item 클릭해서 CalendarActivity로 intent
-            itemBinding.layoutPastItem.setOnClickListener {
-                onClickDetailButton.invoke()
-            }
         }
     }
 
@@ -38,16 +35,17 @@ class PastHabitRecyclerAdapter(
     }
 
     override fun onBindViewHolder(holder: PastHabitViewHolder, position: Int) {
-        holder.bind(pastList[position])
-        Log.d("PAST ADAPTER", pastList.toString())
+        holder.bind(getItem(position))
     }
 
-    override fun getItemCount(): Int {
-        return pastList.size
-    }
+    // Room을 사용할 경우 diffUtil로 notifyFataSetChanged 대체 가능
+    class PastHabitComparator: DiffUtil.ItemCallback<Habit>(){
+        override fun areItemsTheSame(oldItem: Habit, newItem: Habit): Boolean {
+            return oldItem === newItem
+        }
 
-    fun setData(pastHabitList: List<Habit>){
-        pastList = pastHabitList
-        notifyDataSetChanged()
+        override fun areContentsTheSame(oldItem: Habit, newItem: Habit): Boolean {
+            return oldItem.id == newItem.id
+        }
     }
 }
