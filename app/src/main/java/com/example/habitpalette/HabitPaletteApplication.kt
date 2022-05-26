50a80fdcb3c0bd088346bac8d109cd49924b9075
soeun lee <thdmsdl87@gmail.com>
2022-05-26 22:05:09 +0900

    Feat: Add Application Class

--- app/src/main/java/com/example/habitpalette/HabitPaletteApplication.kt
+++ app/src/main/java/com/example/habitpalette/HabitPaletteApplication.kt
@@ -3,12 +3,14 @@ package com.example.habitpalette
 import android.app.Application
 import com.example.habitpalette.data.persistence.AppDatabase
 import com.example.habitpalette.data.repository.HabitRepository
+import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
+@HiltAndroidApp
 class HabitPaletteApplication: Application() {
     val applicationScope = CoroutineScope(SupervisorJob())
 
     val database by lazy {AppDatabase.getDatabase(this, applicationScope)}
-    val repository by lazy {HabitRepository(database!!.HabitDao())}
+    val repository by lazy {HabitRepository(database.HabitDao())}
 }
